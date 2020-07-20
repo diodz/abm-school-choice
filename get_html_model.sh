3190b0d6fe7af7b2c60f06a12c63b35c83207caf
Diego A. Diaz <diegodiaz@uchicago.edu>
2020-07-20 00:35:03 -0500

    Update get_html_model.sh

--- get_html_model.sh
+++ get_html_model.sh
@@ -3,7 +3,7 @@
 
 echo "Getting data files and creating directories..."
 
-mkdir data
+mkdir html
 
-wget --no-check-certificate 'https://drive.google.com/file/d/1usn-23EBkXI8d9H7aivZm-Zzd6IG5smM/view?usp=sharing' -O data/abm_school_choice.html
+wget --no-check-certificate 'https://drive.google.com/file/d/1usn-23EBkXI8d9H7aivZm-Zzd6IG5smM/view?usp=sharing' -O html/abm_school_choice.html
 
